9e63e969d098f14712715d9048d997928c0551fd
Frederick <fedmaster217@yahoo.com>
2019-03-13 15:06:41 -0400

    added search function, updated dashboard, caret button background, updated profile page...

--- js/sb-admin-2.js
+++ js/sb-admin-2.js
@@ -5,6 +5,20 @@
         $(this).find('i').toggleClass('fa-caret-square-down fa-caret-square-up')
     });
     
+    $("#searchBar").keyup(function () {
+        var value = this.value.toLowerCase().trim();
+
+        $("table tr").each(function (index) {
+            if (!index) return;
+            $(this).find("td").each(function () {
+                var id = $(this).text().toLowerCase().trim();
+                var not_found = (id.indexOf(value) == -1);
+                $(this).closest('tr').toggle(!not_found);
+                return not_found;
+            });
+        });
+    });
+    
   // Toggle the side navigation
   $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
     $("body").toggleClass("sidebar-toggled");
